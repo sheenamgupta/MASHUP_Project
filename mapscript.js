b225ce93ae68798648203b8f84ceee7ec4a14387
Sheenam Gupta <744551@student.tor.loc>
2019-04-11 10:55:44 -0400

    milestone 3

--- mapscript.js
+++ mapscript.js
@@ -13,8 +13,13 @@ var mapOptions3={
     zoom: 18,
     center: {lat:40.758904, lng:-73.985128}
 };
+var mapoptions={
+    zoom: 12,
+    center: new google.maps.LatLng(40.758904, -73.985128)
+};
 map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions1);
 map2=new google.maps.Map(document.getElementById('map_canvas2'),mapOptions2);
 map3=new google.maps.Map(document.getElementById('map_canvas3'),mapOptions3);
+map=new google.maps.Map(document.getElementById('map_canvas'),mapOptions);
 }
 google.maps.event.addDomListener(window,'load',initialize);
